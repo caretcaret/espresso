ea0b5014cacd604c15710758f9d81661fd03803c
Jeffrey Zhang <caretcaret@gmail.com>
2015-04-11 04:37:11 -0400

    Softmax evaluation layer
    TODO:
    - saving/loading parameters/models
    - training
    - convolutional layer/pooling
    - generalize building nets into a class

--- src/espresso.cpp
+++ src/espresso.cpp
@@ -12,31 +12,37 @@ int main(int argc, char **argv) {
   std::default_random_engine generator(rd());
 
   int n_input = 5;
-  int n_output = 7;
+  int n_hidden = 7;
+  int n_classes = 3;
 
   // construct abstract network
-  Halide::ImageParam W(Halide::type_of<double>(), 2);
-  Halide::ImageParam b(Halide::type_of<double>(), 1);
   Halide::ImageParam input(Halide::type_of<double>(), 1);
-  Halide::Func layer = Espresso::hidden_layer(Halide::Func(input), Halide::Func(W), Halide::Func(b), n_input, n_output);
+  Halide::ImageParam W1(Halide::type_of<double>(), 2);
+  Halide::ImageParam b1(Halide::type_of<double>(), 1);
+  Halide::ImageParam W2(Halide::type_of<double>(), 2);
+  Halide::ImageParam b2(Halide::type_of<double>(), 1);
+
+  Halide::Func layer1 = Espresso::hidden_layer(Halide::Func(input), Halide::Func(W1), Halide::Func(b1), n_input, n_hidden);
+  Halide::Func layer2 = Espresso::softmax(layer1, Halide::Func(W2), Halide::Func(b2), n_hidden, n_classes);
 
   // instantiate inputs
-  Halide::Image<double> W_(n_output, n_input);
-  Halide::Image<double> b_(n_output);
   Halide::Image<double> input_(n_input);
-  Espresso::fill_random(W_, generator, 0.0, 1.0);
-  Espresso::fill_random(b_, generator, 0.0, 1.0);
+  Halide::Image<double> W1_(n_hidden, n_input), W2_(n_classes, n_hidden);
+  Halide::Image<double> b1_(n_hidden), b2_(n_classes);
   Espresso::fill_random(input_, generator, 0.0, 1.0);
-  std::cout << "W:" << std::endl << W_ << std::endl;
-  std::cout << "b:" << std::endl << b_ << std::endl;
-  std::cout << "input:" << std::endl << input_ << std::endl;
+  Espresso::fill_random(W1_, generator, 0.0, 1.0);
+  Espresso::fill_random(b1_, generator, 0.0, 1.0);
+  Espresso::fill_random(W2_, generator, 0.0, 1.0);
+  Espresso::fill_random(b2_, generator, 0.0, 1.0);
 
-  W.set(W_);
-  b.set(b_);
   input.set(input_);
+  W1.set(W1_);
+  b1.set(b1_);
+  W2.set(W2_);
+  b2.set(b2_);
 
   // JIT compile and run
-  Halide::Image<double> output = layer.realize(n_output);
+  Halide::Image<double> output = layer2.realize(n_classes);
 
   std::cout << "output:" << std::endl << output << std::endl;
 
--- src/net_components.h
+++ src/net_components.h
@@ -20,6 +20,18 @@ Halide::Func hidden_layer(Halide::Func input, Halide::Func W, Halide::Func b, in
     return output;
 }
 
+Halide::Func softmax(Halide::Func input, Halide::Func W, Halide::Func b, int n_input, int n_output, float epsilon=1e-8) {
+    Halide::Func activation("activation"), output("output");
+    Halide::Expr normalizer;
+    Halide::Var i;
+    Halide::RDom r(0, n_input), s(0, n_output);
+
+    activation(i) = Halide::exp(Halide::sum(W(i, r.x) * input(r.x)) + b(i));
+    normalizer = Halide::sum(activation(s.x)) + epsilon;
+    output(i) = activation(i) / normalizer;
+    return output;
 }
+
+} // namespace
 
 #endif // ESPRESSO_NET_COMPONENTS_H
